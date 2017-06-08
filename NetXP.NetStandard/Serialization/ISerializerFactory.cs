ec9dbd93fc4a59d31f9360bb8fb4d0efc4c7d04d
juandrn <juan.drn@yandex.com>
2017-06-08 12:48:10 -0400

    new ISerializerFactory.cs

---
+++ NetXP.NetStandard/Serialization/ISerializerFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetXP.NetStandard.Serialization
+{
+    public interface ISerializerFactory
+    {
+        ISerializer Resolve(SerializerType serializeType);
+    }
+}
