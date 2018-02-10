c33897778eafdca98fd03176c8b453c65d22278e
root <root@localhost.localdomain>
2018-02-09 22:41:57 -0300

    SystemInformer implementation for linux

---
+++ NetXP.NetStandard/Exceptions/NetXPException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetXP.NetStandard.Exceptions
+{
+    public class NetXPApplicationException : Exception
+    {
+        public NetXPApplicationException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
--- NetXP.NetStandard/SystemInformation/Implementations/ServiceInformer.cs
+++ NetXP.NetStandard/SystemInformation/Implementations/ServiceInformer.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using NetXP.NetStandard.Exceptions;
+using NetXP.NetStandard.Processes;
 
 namespace NetXP.NetStandard.SystemInformation.Implementations
 {
     public class ServiceInformer : IServiceInformer
     {
         private readonly ISystemInformation systemInformation;
+        private readonly IIOTerminal terminal;
 
-        public ServiceInformer(ISystemInformation systemInformation)
+        public ServiceInformer(
+            ISystemInformation systemInformation,
+            IIOTerminal terminal
+        )
         {
             this.systemInformation = systemInformation;
+            this.terminal = terminal;
         }
 
         public List<ServiceInformation> GetServices()
@@ -32,7 +40,32 @@ namespace NetXP.NetStandard.SystemInformation.Implementations
             }
             else if (osInfo.Platform == OSPlatformType.Linux)
             {
-                throw new NotImplementedException();
+                ProcessOutput output = terminal.Execute(new ProcessInput
+                {
+                    Command = "systemctl list-unit-files --all --type=service | grep ''",
+                    ShellName = "/bin/bash",
+                    MaxOfSecondToWaitCommand = 5,
+                    Arguments = ""
+                });
+
+                foreach (var line in output.StandardOutput.Skip(1))
+                {
+                    if (!line.Contains("service"))
+                    {
+                        continue;
+                    }
+                    var lineSplited = line.Split(new char[] { ' ' }, 2, StringSplitOptions.RemoveEmptyEntries);
+                    if (lineSplited.Length != 2)
+                    {
+                        throw new SystemInformationException("Service Output Failed, The systemctl Output has changed");
+                    }
+
+                    servicesInformations.Add(new ServiceInformation
+                    {
+                        ServiceName = lineSplited[0]
+                    });
+                }
+
             }
 
             return servicesInformations.Count > 0 ? servicesInformations : null;
