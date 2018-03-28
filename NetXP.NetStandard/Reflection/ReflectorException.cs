8d695e441f9604980c8fbda3df6e691dd498c077
juandrn <juan.drn@yandex.com>
2018-03-28 20:28:39 -0300

    - Reflector exception added.

---
+++ NetXP.NetStandard/Reflection/ReflectorException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace NetXP.NetStandard.Reflection
+{
+    [Serializable]
+    internal class ReflectorException : Exception
+    {
+        public ReflectorException()
+        {
+        }
+
+        public ReflectorException(string message) : base(message)
+        {
+        }
+
+        public ReflectorException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ReflectorException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
