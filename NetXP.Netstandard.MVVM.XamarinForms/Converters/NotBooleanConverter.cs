4516c5c9ff8d42484e5e860562940e0579e47f6e
juandrn <juan.drn@yandex.com>
2019-02-15 18:21:17 -0300

    Converters IsNotNullOrEmptyStringConverter and NotBooleanConverter.cs

---
+++ NetXP.Netstandard.MVVM.XamarinForms/Converters/NotBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace NetXP.NetStandard.MVVM.XamarinForms.Converters
+{
+    public class NotBooleanConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(bool)) throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+
+    }
+}
